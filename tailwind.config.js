3d2d72037793c38794b87f56a430c9004eeb0417
Shema <shemaphilbert8@gmail.com>
2025-02-02 22:07:37 +0200

    full functioning header

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./html/*.{html,js}"],
+  content: [
+    "./html/*.{html,js}",
+   
+  ],
   theme: {
+    
     extend: {
-      backgroundImage: {
-        'custom-gradient': 'linear-gradient(to right, rgb(37, 99, 235 , 0) 0%, transparent 100%)',
-      },
+     
+    },
   },
   plugins: [],
 }
